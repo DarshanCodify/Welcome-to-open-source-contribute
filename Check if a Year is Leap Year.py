1b8dbc0d4a002be902940d84a28e30cfa7a0a35b
Darshan R <156777489+DarshanCodify@users.noreply.github.com>
2024-04-30 23:37:49 +0530

    Add files via upload

---
+++ Check if a Year is Leap Year.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Program to check if a year is a leap year
+
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+year = int(input("Enter a year: "))
+
+if is_leap_year(year):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
+
+
+# In[ ]:
+
+
+
+
